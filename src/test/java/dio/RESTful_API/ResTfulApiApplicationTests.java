49adca984aaac21c2992a840089cf03563066fea
Edson Sanches <edsons@RM.GROUP>
2025-03-13 10:22:36 -0300

    Inicia o projeto com o Spring initializr

---
+++ src/test/java/dio/RESTful_API/ResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.RESTful_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
